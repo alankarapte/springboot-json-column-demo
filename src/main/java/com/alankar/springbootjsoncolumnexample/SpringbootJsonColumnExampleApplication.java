79b920e09f16a1829d2b143e7587d724815f9f53
alankarapte <alankar@vedirasolutions.com>
2021-02-02 12:52:07 +0530

    initial commit

---
+++ src/main/java/com/alankar/springbootjsoncolumnexample/SpringbootJsonColumnExampleApplication.java
@@ -0,0 +1,13 @@
+package com.alankar.springbootjsoncolumnexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJsonColumnExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJsonColumnExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alankar/springbootjsoncolumnexample/SpringbootJsonColumnExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alankar.springbootjsoncolumnexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJsonColumnExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
